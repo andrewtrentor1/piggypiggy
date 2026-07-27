19433bc36bb5b5e71ee5f7f1a9015e79f3a2189f
drew <andrew@minnesotapb.com>
2026-07-27 14:58:20 -0500

    Golf: settle hole rules and CTP carryovers

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // MBE PIG POINTS - Service Worker
 // Handles offline support, caching, and push notifications
 
-const CACHE_NAME = 'mbe-pig-points-v22-scorecards-lots-privacy';
+const CACHE_NAME = 'mbe-pig-points-v23-golf-rulebook';
 const urlsToCache = [
   '/',
   '/index.html',
